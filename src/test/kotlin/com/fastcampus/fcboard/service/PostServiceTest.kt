abbd993e324c78f3d3fa1f2e760322c86c0b4fb5
sangkukbae <sangkukbae@gmail.com>
2025-11-23 11:35:07 +0900

    feat: Retrieve comments with post details in PostService
    - Updated `PostService` to include comments when retrieving post details.
    - Enhanced `PostDetailResponse` and related DTOs to map and include comments.
    - Added unit tests in `PostServiceTest` to verify comments are properly fetched with posts.

--- src/test/kotlin/com/fastcampus/fcboard/service/PostServiceTest.kt
+++ src/test/kotlin/com/fastcampus/fcboard/service/PostServiceTest.kt
@@ -1,9 +1,11 @@
 package com.fastcampus.fcboard.service
 
+import com.fastcampus.fcboard.domain.Comment
 import com.fastcampus.fcboard.domain.Post
 import com.fastcampus.fcboard.exception.PostNotDeletableException
 import com.fastcampus.fcboard.exception.PostNotFoundException
 import com.fastcampus.fcboard.exception.PostNotUpdatableException
+import com.fastcampus.fcboard.repository.CommentRepository
 import com.fastcampus.fcboard.repository.PostRepository
 import com.fastcampus.fcboard.service.dto.PostCreateRequestDto
 import com.fastcampus.fcboard.service.dto.PostSearchRequestDto
@@ -21,9 +23,10 @@ import org.springframework.data.repository.findByIdOrNull
 
 @SpringBootTest
 class PostServiceTest(
-    private val postService: PostService,
-    @Autowired private val postRepository: PostRepository,
-    repository: PostRepository,
+    @Autowired private val postService: PostService,
+    private val postRepository: PostRepository,
+    private val commentRepository: CommentRepository,
+    service: PostService,
 ) : BehaviorSpec({
     beforeSpec {
         postRepository.saveAll(
@@ -152,6 +155,39 @@ class PostServiceTest(
                 shouldThrow<PostNotFoundException> {
                     postService.getPost(9999L)
                 }
+            }
+        }
+        When("댓글 추가시") {
+            commentRepository.save(
+                Comment(
+                    "댓글 내용1",
+                    saved,
+                    "댓글 작성자"
+                )
+            )
+            commentRepository.save(
+                Comment(
+                    "댓글 내용2",
+                    saved,
+                    "댓글 작성자"
+                )
+            )
+            commentRepository.save(
+                Comment(
+                    "댓글 내용3",
+                    saved,
+                    "댓글 작성자"
+                )
+            )
+            val post = postService.getPost(saved.id)
+            then("댓글이 함께 조회됨을 확인한다.") {
+                post.comments.size shouldBe 3
+                post.comments[0].content shouldBe "댓글 내용1"
+                post.comments[1].content shouldBe "댓글 내용2"
+                post.comments[2].content shouldBe "댓글 내용3"
+                post.comments[0].createdBy shouldBe "댓글 작성자"
+                post.comments[1].createdBy shouldBe "댓글 작성자"
+                post.comments[2].createdBy shouldBe "댓글 작성자"
             }
         }
     }
